8478ecbb710a7b9920ca86c075eee3af919861c2
Dustin Muse <Dustin.muse12@gmail.com>
2023-11-08 19:53:53 -0500

    initialization of tests

---
+++ TestClassScheduling.py
@@ -0,0 +1,19 @@
+'''
+Testing class
+'''
+
+import unittest
+
+class TestClassScheduling():
+    def testShowCourses(self):
+        pass
+    def testNumberOfCourses(self):
+        pass
+    def testEnterCourses(self):
+        pass
+    def testShownSchedule(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
