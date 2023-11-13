7b491e5bdffc17d33ac760e93206d8af4629830f
Dustin Muse <Dustin.muse12@gmail.com>
2023-11-13 14:56:22 -0500

    3 out of 4 test cases finished

--- TestClassScheduling.py
+++ TestClassScheduling.py
@@ -7,28 +7,30 @@ as I would like to be consistent and not make it ugly
 import unittest
 from Scheduling import CourseScheduler
 
-class TestClassScheduling():
+class TestClassScheduling(unittest.TestCase):
   
     def setUp(self):
         self.scheduler = CourseScheduler()
         self.scheduler.load_courses_from_file('mock_courses.txt')  # Assumption that the method loads courses from a file
 
-    def test_display_all_courses(self):
+    def test_display_all_courses(self): #FR1
         # Test to verify if all courses are displayed correctly
         expected_output = [...]  # Expected format and content of the course list
         self.assertEqual(self.scheduler.display_courses(), expected_output)
 
-    def test_enter_number_of_courses(self):
+    def test_enter_number_of_courses(self): #FR2
         # Test to verify if the courses entered are valid
         self.assertTrue(self.scheduler.set_course_count(3))  # Assuming that the input was a valid input
         self.assertRaises(ValueError, self.scheduler.set_course_count, -1)  # For Negative numbers
         self.assertRaises(TypeError, self.scheduler.set_course_count, 'a')  # For Non-numeric/string input
 
-    def testEnterCourses(self):
+    def test_enter_courses(self): #FR3
         pass
       
-    def testShownSchedule(self): #FR4
-        pass
+    def test_shown_schedule(self, course): #FR4
+        self.scheduler.add_to_schedule(course)
+        self.assertEqual(self.scheduler.get_schedule(), course)
+        self.assertEqual(self.scheduler.get_schedule(), "", "There is no schedule found")
 
 
 if __name__ == '__main__': #runs tests
