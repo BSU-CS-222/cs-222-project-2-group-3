f234b7a890130e512354d6299482f0babe361899
Jadi Miller <jadilmiller@gmail.com>
2023-11-21 11:40:45 -0500

    Add text_file_to_dictionary()

--- Scheduling.py
+++ Scheduling.py
@@ -6,4 +6,24 @@ class CourseScheduler:
     courses = "CoursesDemo.txt"
     fileData = open(courses, "rt")
     courseData = fileData.readlines()
+    text_file_to_dictionary()
     fileData.close()
+
+def text_file_to_dictionary():
+    classes_dict = {}
+    parts = courseData.split(" ")
+    course_number = parts[0]
+    course_section = parts[1]
+    days = days[2]
+    start_time = [3]
+
+
+    classes_dict[] = {
+        'course_number': course_number,
+        'course_section': course_section,
+        'days': days,
+        'start_time': start_time,
+        'end_time': end_time
+    }
+
+    return classes_dict
