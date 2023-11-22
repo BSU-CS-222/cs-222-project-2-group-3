948d5b12649ab2100da4a70c6550b3ecc9158077
Naqi404 <112116609+Naqi404@users.noreply.github.com>
2023-11-22 15:33:04 -0500

    Fixed the problem in the main file with the txt to dict converter and changed variable names to be consistent with the rest of the code

--- Scheduling.py
+++ Scheduling.py
@@ -4,19 +4,33 @@ Where all our methods/functions go
 
 
 class Scheduling:
-    def __init__(self, courses_file = "CoursesDemo.txt"):
-        self.courses = courses_file
-        self.fileData = open(self.courses, "rt") # opens file
-        self.courseData = self.fileData.readlines() # reads the file
-        self.fileData.close()
+    def __init__(self, courses_file="CoursesDemo.txt"):
+        self.courses_file = courses_file
+        self.course_data = self.read_courses_file()
 
-    def text_file_to_dictionary(self, courseData):
+    def read_courses_file(self):
+        with open(self.courses_file, "rt") as file:
+            return file.readlines()
+
+    def text_file_to_dictionary(self):
         classes_dict = {}
-        for line in courseData: # reads each line in the file
-            classes_dict = {"course_number" : line} # this is only returning the last line (problem)
-
+        for line in self.course_data:
+            # Over here this function will split each line into parts. So something like (course number, days, times) 
+            course_parts = line.strip().split()  # This assumes the file has space-separated values since we know it does
+            # Check if the line has the right amount of parts (which in our case is a min of 5
+            if len(course_parts) >= 5:
+                # Create a unique key for each course (like "CS120-001")
+                course_key = f"{course_parts[0]}-{course_parts[1]}"
+                # Store course details in the dictionary under the unique key
+                classes_dict[course_key] = {
+                    "course_number": course_parts[0],
+                    "section": course_parts[1],
+                    "days": course_parts[2],
+                    "start_time": course_parts[3],
+                    "end_time": course_parts[4]
+                }
         return classes_dict
-
+    
 
 # Example usage
 scheduling_instance = Scheduling()
