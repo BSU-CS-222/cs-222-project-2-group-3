67b1a1acccd704f5d27429b4d35bcc73e46c2d1a
Jadi Miller <jadilmiller@gmail.com>
2023-12-03 20:20:20 -0500

    Recieve user input for number of courses and course codes

--- Scheduling.py
+++ Scheduling.py
@@ -1,10 +1,5 @@
-'''
-Where all our methods/functions go
-'''
-
-
 class Scheduling:
-    def __init__(self, courses_file = "CoursesDemo.txt"):
+    def __init__(self, courses_file="courses.txt"):
         self.courses_file = courses_file
         self.course_data = self.read_courses_file()
 
@@ -31,7 +26,50 @@ class Scheduling:
                 }
         return classes_dict
     
+    def generate_schedule(self, selected_courses):
+        schedule = {}
+        for course_key in selected_courses:
+            # Stripping whitespaces and making the comparison case-insensitive
+            course_key = course_key.strip().upper()
+
+            # Generate the expected course key for comparison
+            expected_course_key = f"{course_key.split('-')[0]}-{course_key.split('-')[1]}"
+
+            # Convert the course data to a dictionary
+            course_data_dict = self.text_file_to_dictionary()
+
+            if expected_course_key in course_data_dict:  # Check if the entered key is in the course data
+                schedule[expected_course_key] = course_data_dict[expected_course_key]
+            else:
+                print(f"Course {course_key} not found in the schedule.")
+        return schedule
+
+
+    def register_courses(self):
+        print("Available Courses:")
+        for course_key, details in self.text_file_to_dictionary().items():
+            print(f"{course_key} - {details['days']}, Start: {details['start_time']}, End: {details['end_time']}")
+            # Formatting for displaying all courses
+
+        n = int(input("Enter how many courses you would like to register for: "))
+        selected_courses = []
+        for i in range(n):
+            course_key = input(f"Enter course {i + 1} code (e.g., CS120-001): ")
+            selected_courses.append(course_key)
+
+        schedule = self.generate_schedule(selected_courses)
+
+        if schedule:
+            print("\nYour Schedule:")
+            for course_key, course in schedule.items():
+                print(f"Course: {course['course_number']} - Section: {course['section']}")
+                print(f"Days: {course['days']} | Time: {course['start_time']}-{course['end_time']}\n")
+        else:
+            print("No schedule found for the selected courses.")
+
 
 # Example usage
 scheduling_instance = Scheduling()
-print(scheduling_instance.text_file_to_dictionary())
+scheduling_instance.register_courses()
+
+
