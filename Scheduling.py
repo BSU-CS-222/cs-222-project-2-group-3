823c7532bc324319f2ea16cd718d806fecaed5fc
Naqi404 <112116609+Naqi404@users.noreply.github.com>
2023-12-04 22:47:34 -0500

    Refactored Scheduling into 3 classes for SRP, refactored test suite, added mock file

--- Scheduling.py
+++ Scheduling.py
@@ -1,66 +1,51 @@
-class Scheduling:
-    def __init__(self, courses_file="courses.txt"):
-        self.courses_file = courses_file
-        self.course_data = self.read_courses_file()
+class Course:
+    def __init__(self, course_number, section, days, start_time, end_time):
+        self.course_number = course_number
+        self.section = section
+        self.days = days
+        self.start_time = start_time
+        self.end_time = end_time
 
+class CourseManager:
+    def __init__(self):
+        self.courses = []
 
-    # Reads the specified file and returns a list of strings
-    def read_courses_file(self):
-        with open(self.courses_file, "rt") as file:
-            return file.readlines()
+    def load_courses(self, courses_file="courses.txt"):
+        with open(courses_file, "rt") as file:
+            for line in file:
+                course_parts = line.strip().split()
+                if len(course_parts) >= 5:
+                    course_key = f"{course_parts[0]}-{course_parts[1]}"
+                    # Check if the course is not already in the list before appending
+                    if not any(course.course_number == course_key for course in self.courses):
+                        course = Course(course_parts[0], course_parts[1], course_parts[2], course_parts[3], course_parts[4])
+                        self.courses.append(course)
 
+    def find_course(self, course_key):
+        for course in self.courses:
+            if f"{course.course_number}-{course.section}" == course_key:
+                return course
+        return None
 
-    # Takes course_data list, splits it into parts, and creates a dictionary
-    def text_file_to_dictionary(self):
-        classes_dict = {}
-        for line in self.course_data:
-            # Over here this function will split each line into parts. So something like (course number, days, times) 
-            course_parts = line.strip().split()  # This assumes the file has space-separated values since we know it does
-            # Check if the line has the right amount of parts (which in our case is a min of 5
-            if len(course_parts) >= 5:
-                # Create a unique key for each course (like "CS120-001")
-                course_key = f"{course_parts[0]}-{course_parts[1]}"
-                # Store course details in the dictionary under the unique key
-                classes_dict[course_key] = {
-                    "course_number": course_parts[0],
-                    "section": course_parts[1],
-                    "days": course_parts[2],
-                    "start_time": course_parts[3],
-                    "end_time": course_parts[4]
-                }
-        return classes_dict
-    
+class Scheduler:
+    def __init__(self):
+        self.course_manager = CourseManager()
 
-    # Takes list of selected course keys and checks if they exist in the course data dictionary. 
-    # If found, they are added to the schedule dictionary.
+    def display_available_courses(self):
+        print("Available courses:")
+        for course in self.course_manager.courses:
+            print(f"{course.course_number}-{course.section} | {course.days} | Start: {course.start_time}, End: {course.end_time}")
+
     def generate_schedule(self, selected_courses):
-        schedule = {}
+        schedule = []
         for course_key in selected_courses:
-            # Case-insensitivity and stripping white space
-            course_key = course_key.strip().upper()
-
-            # Generate expected course key for comparison
-            expected_course_key = f"{course_key.split('-')[0]}-{course_key.split('-')[1]}"
-
-            # Convert the course data to a dictionary
-            course_data_dict = self.text_file_to_dictionary()
-
-            if expected_course_key in course_data_dict:  # Check if entered key is in course_data
-                schedule[expected_course_key] = course_data_dict[expected_course_key]
+            course = self.course_manager.find_course(course_key)
+            if course:
+                schedule.append(course)
             else:
                 print(f"Course {course_key} not found in the schedule.")
         return schedule
 
-
-    # Prints the available courses and prompts user for number of courses to register. 
-    def display_available_courses(self):
-        print("Available courses:")
-        for course_key, details in self.text_file_to_dictionary().items():
-            print(f"{course_key} | {details['days']} | Start: {details['start_time']}, End: {details['end_time']}")
-            # Formatting for displaying all courses
-
-
-    # Collects the course keys from the user and generates their schedule.
     def register_courses(self):
         n = int(input("Enter how many courses you would like to register for: "))
         selected_courses = []
@@ -72,16 +57,14 @@ class Scheduling:
 
         if schedule:
             print("\nYour Schedule:")
-            for course_key, course in schedule.items():
-                print(f"Course: {course['course_number']} - Section: {course['section']}")
-                print(f"Days: {course['days']} | Time: {course['start_time']}-{course['end_time']}\n")
+            for course in schedule:
+                print(f"Course: {course.course_number} - Section: {course.section}")
+                print(f"Days: {course.days} | Time: {course.start_time}-{course.end_time}\n")
         else:
             print("No schedule found for the selected courses.")
 
-
 # Example usage
-scheduling_instance = Scheduling()
-scheduling_instance.display_available_courses()
-scheduling_instance.register_courses()
-
-
+scheduler_instance = Scheduler()
+scheduler_instance.course_manager.load_courses()
+scheduler_instance.display_available_courses()
+scheduler_instance.register_courses()
--- test_classScheduling.py
+++ test_classScheduling.py
@@ -1,50 +1,56 @@
-'''
-Testing class
-Notes: I personally prefer snake_case over camelCase which is how I changed the tests, if you prefer one naming convention over the other let's go with that
-as I would like to be consistent and not make it ugly
-
-add chck for duplicates
-
-'''
-
 import unittest
-from Scheduling import Scheduling
+from unittest.mock import patch
+from Scheduling import Course, CourseManager, Scheduler
 
+class TestCourse(unittest.TestCase):
+    def test_course_creation(self):
+        course = Course("CS120", "001", "MWF", "0900", "0950")
+        self.assertEqual(course.course_number, "CS120")
+        self.assertEqual(course.section, "001")
+        self.assertEqual(course.days, "MWF")
+        self.assertEqual(course.start_time, "0900")
+        self.assertEqual(course.end_time, "0950")
 
-class TestClassScheduling(unittest.TestCase):
-
+class TestCourseManager(unittest.TestCase):
     def setUp(self):
-        self.scheduler = Scheduling()
-        self.scheduler.load_courses_from_file()  # Assumption that the method loads courses from a file
+        self.course_manager = CourseManager()
 
-    def test_display_all_courses(self):  # FR1
-        # Test to verify if all courses are displayed correctly
-        expected_output = ['CS120', 'CS121', 'CS120', 'CS222']  # Expected format and content of the course list
-        self.assertEqual(self.scheduler.display_courses(), expected_output)
+    def test_load_courses(self):
+        # Create a mock courses file
+        with open("mock_courses.txt", "w") as file:
+            file.write("CS120 001 MWF 0900 0950\n")
 
-    def test_enter_number_of_courses(self):  # FR2
-        # Test to verify if the courses entered are valid
-        self.assertTrue(self.scheduler.set_course_count(3))  # Assuming that the input was a valid input
-        with self.assertRaises(ValueError):  # For Negative numbers
-            self.scheduler.set_course_count(-1)
-        with self.assertRaises(ValueError):  # For Non-numeric/string input
-            self.scheduler.set_course_count('a')
+        self.course_manager.load_courses("mock_courses.txt")
+        self.assertEqual(len(self.course_manager.courses), 1)
 
-    def test_enter_courses(self):  # FR3
-        # Test to verify if entered courses are stored correctly
-        entered_courses = ['CS120', 'CS121', 'CS222']  # List of courses entered by the user
-        self.scheduler.enter_courses(entered_courses)
-        self.assertEqual(self.scheduler.get_user_courses(), entered_courses)
+    def test_find_course(self):
+        course = Course("CS120", "001", "MWF", "0900", "0950")
+        self.course_manager.courses.append(course)
 
-    def test_shown_schedule(self):  # FR4
-        self.assertEqual(self.scheduler.get_schedule(), {}, "There is no schedule found")  # checks to see if there is no schedule
-        course = {'course_number': 'CS120', 'course_section': '001', 'days': 'MWF', 'start_time': '0900', 'end_time': '0950'}
-        self.scheduler.add_to_schedule(course)
-        self.assertEqual(self.scheduler.get_schedule(), course)  # makes sure the schedule will show when added
+        found_course = self.course_manager.find_course("CS120-001")
+        self.assertEqual(found_course, course)
 
-    def test_check_for_duplicates(self):
-        entered_courses = ['CS120', 'CS121', 'CS222']  # List of courses entered by the user
-        self.assertTrue(self.scheduler.check_for_duplicates(entered_courses))  # makes sure that the courses shouldn't be the same
+        not_found_course = self.course_manager.find_course("CS121-001")
+        self.assertIsNone(not_found_course)
 
-if __name__ == '__main__': #runs tests
+class TestScheduler(unittest.TestCase):
+    def setUp(self):
+        self.scheduler = Scheduler()
+
+    def test_generate_schedule(self):
+        course = Course("CS120", "001", "MWF", "0900", "0950")
+        self.scheduler.course_manager.courses.append(course)
+
+        selected_courses = ["CS120-001"]
+        schedule = self.scheduler.generate_schedule(selected_courses)
+
+        self.assertEqual(len(schedule), 1)
+        self.assertEqual(schedule[0], course)
+
+    @patch('builtins.input', side_effect=['2', 'CS120-001', 'CS121-002'])
+    def test_register_courses(self, mock_input):
+        self.scheduler.course_manager.load_courses("mock_courses.txt")
+        self.scheduler.register_courses()
+
+if __name__ == '__main__':
     unittest.main()
